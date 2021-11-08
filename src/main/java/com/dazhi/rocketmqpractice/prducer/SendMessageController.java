f8daa318d0355e0b202b9b0b7814d67b3d0a3059
zhiyong.guo <zhiyong.guo@bioknow.net>
2021-11-08 10:33:23 +0800

    新建生产者和消费者

--- src/main/java/com/dazhi/rocketmqpractice/prducer/SendMessageController.java
+++ src/main/java/com/dazhi/rocketmqpractice/prducer/SendMessageController.java
@@ -1,4 +1,21 @@
 package com.dazhi.rocketmqpractice.prducer;
 
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("rocket")
 public class SendMessageController {
+
+    @Autowired
+    private RocketMQTemplate rocketMQTemplate;
+
+    @GetMapping
+    public String sendSimpleMessage() {
+        rocketMQTemplate.convertAndSend("TopicTest","这是一条测试信息");
+        return "SUCCESS";
+    }
 }
